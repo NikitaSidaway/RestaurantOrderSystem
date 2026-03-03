23d526785eacf45c10802102ff38055f46d18068
NikitaSidaway <25400@burnside.school.nz>
2026-03-04 09:52:29 +1300

    Set up route to cashier_screen

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+@app.route("/")
+def cashier_screen():
+    return render_template("cashier_screen.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
